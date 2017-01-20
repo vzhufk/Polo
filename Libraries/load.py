103e73052e2228952867f597560edbe5789c8a2d
Vadym Zhufyak <zhufyakvv@gmail.com>
2017-01-20 05:25:47 +0200

    redesign

--- Libraries/load.py
+++ Libraries/load.py
@@ -6,16 +6,16 @@ import pygame
 def image(name, color_key=None):
     fullname = os.path.join('', name)
     try:
-        image = pygame.image.load(fullname)
+        current = pygame.image.load(fullname)
     except pygame.error as message:
         print('Cannot load image:', name)
         raise SystemExit(message)
-    image = image.convert()
+    current = current.convert()
     if color_key is not None:
         if color_key is -1:
-            color_key = image.get_at((0, 0))
-        image.set_colorkey(color_key, pygame.RLEACCEL)
-    return image, image.get_rect()
+            color_key = current.get_at((0, 0))
+        current.set_colorkey(color_key, pygame.RLEACCEL)
+    return current, current.get_rect()
 
 
 def load_lined_sprite(filename, w, h):
--- Modules/tile.py
+++ Modules/tile.py
@@ -13,7 +13,8 @@ class Tile(pygame.sprite.Sprite):
     def __init__(self, name, placement=(0, 0)):
         pygame.sprite.Sprite.__init__(self)
         self.name = name
-        self.image, self.rect = load.image(name, -1)
+        # COLOR KEY ROLLS
+        self.image, self.rect = load.image(name, (0, 0, 0, 255))
         self.original = self.image
         self.rect = Rect(placement[0], placement[1], self.rect[0] + self.rect[2],
                          self.rect[1] + self.rect[3])
